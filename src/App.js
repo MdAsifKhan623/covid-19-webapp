c8b657f0109fe9107a588e3d9053841552ffafd3
Asif623 <mkhan135@fiu.edu>
2020-12-03 17:01:02 -0500

    added public url

--- src/App.js
+++ src/App.js
@@ -21,19 +21,19 @@ function App() {
           <Header name="Covid-19 App"/>
           </header>
           <Switch>  
-            <Route path="/" exact render={props=>
+            <Route path={process.env.PUBLIC_URL + '/'} exact render={props=>
             <div>
               <Dashboard/>
             </div>
             }/>
-            <Route path="/news" exact  component={News}/>
-            <Route path="/countries" exact component={Countries}/>
-            <Route path="/continents" exact component={Continents}/>
-            <Route path="/county" exact component={LocalInfo}/>
-            <Route path="/usstates" exact component={USStates}/>
-            <Route path="/testing-sites" exact component={TestingSites}/>
-            <Route path="/vaccine-news" exact component={Vaccine}/>
-            <Route path='/about' exact component={About}/>
+            <Route path={process.env.PUBLIC_URL + '/news'} exact  component={News}/>
+            <Route path={process.env.PUBLIC_URL + '/countries'} exact component={Countries}/>
+            <Route path={process.env.PUBLIC_URL + "/continents"} exact component={Continents}/>
+            <Route path={process.env.PUBLIC_URL + "/county"} exact component={LocalInfo}/>
+            <Route path={process.env.PUBLIC_URL + "/usstates"} exact component={USStates}/>
+            <Route path={process.env.PUBLIC_URL + "/testing-sites"} exact component={TestingSites}/>
+            <Route path={process.env.PUBLIC_URL + "/vaccine-news"} exact component={Vaccine}/>
+            <Route path={process.env.PUBLIC_URL + '/about'} exact component={About}/>
           </Switch>
     </div>
     </Router>

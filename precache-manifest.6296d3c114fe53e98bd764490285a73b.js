ddbc3440e639462ed33368fc92969f755aaf940b
Asif623 <mkhan135@fiu.edu>
2020-12-03 16:43:45 -0500

    Updates

---
+++ precache-manifest.6296d3c114fe53e98bd764490285a73b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb91804fa203ae04789156955450b32",
+    "url": "/covid-19-webapp/index.html"
+  },
+  {
+    "revision": "43f1759a48c3dac5286c",
+    "url": "/covid-19-webapp/static/css/2.330301e3.chunk.css"
+  },
+  {
+    "revision": "5b10f4425eb7fb4d920f",
+    "url": "/covid-19-webapp/static/css/main.f5f8e4bb.chunk.css"
+  },
+  {
+    "revision": "43f1759a48c3dac5286c",
+    "url": "/covid-19-webapp/static/js/2.7cc04d31.chunk.js"
+  },
+  {
+    "revision": "c29bf2bc297e5b651e9fc4fba4b356f1",
+    "url": "/covid-19-webapp/static/js/2.7cc04d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b10f4425eb7fb4d920f",
+    "url": "/covid-19-webapp/static/js/main.8d57a10a.chunk.js"
+  },
+  {
+    "revision": "76435e7553c34751e6f6",
+    "url": "/covid-19-webapp/static/js/runtime-main.a9fc493a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covid-19-webapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covid-19-webapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covid-19-webapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covid-19-webapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covid-19-webapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);

58a658a1581b5578221ac29da5ad8f1b9107d8c3
Asif623 <mkhan135@fiu.edu>
2020-12-04 01:44:25 -0500

    added routing paths

--- src/views/header.js
+++ src/views/header.js
@@ -7,13 +7,13 @@ export default function Header(props){
             <Navbar bg="dark" variant="dark" className="nav-header">
                 <Navbar.Brand href="#home" style={{"fontFamily":"sans-serif"}}>{props.name}</Navbar.Brand>
                 <Nav className="mr-auto">
-                <Nav.Link href="/covid-19-webapp" className='tabs-section'>Home</Nav.Link>
-                <Nav.Link href="covid-19-webapp/continents" className='tabs-section'>Continents</Nav.Link>
-                <Nav.Link href="covid-19-webapp/countries" className='tabs-section'>Countries</Nav.Link>
-                <Nav.Link href="covid-19-webapp/news" className='tabs-section'>News</Nav.Link>
-                <Nav.Link href="covid-19-webapp/county" className='tabs-section'>Local County Info</Nav.Link>
-                <Nav.Link href="covid-19-webapp/usstates" className='tabs-section'>US States</Nav.Link>
-                <Nav.Link href="covid-19-webapp/testing-sites" className='tabs-section'>Testing Sites</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + "/"} className='tabs-section'>Home</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + "/continents"} className='tabs-section'>Continents</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + '/countries'} className='tabs-section'>Countries</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + '/news'} className='tabs-section'>News</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + "/county"} className='tabs-section'>Local County Info</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + "/usstates"} className='tabs-section'>US States</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + "/testing-sites"} className='tabs-section'>Testing Sites</Nav.Link>
                 </Nav>
                 <Form inline>
                 {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" />

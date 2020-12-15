6d67c31316e5bdeb54768b8c5ff7edb45491d218
Asif623 <mkhan135@fiu.edu>
2020-12-15 00:07:20 -0500

    added the path correctly in navigation

--- src/views/header.js
+++ src/views/header.js
@@ -10,11 +10,11 @@ export default function Header(props){
                 <Nav className="mr-auto">
                 <Nav.Link href={process.env.PUBLIC_URL} className='tabs-section'>Home</Nav.Link>
                 <Nav.Link href={process.env.PUBLIC_URL + `/#/continents`} className='tabs-section'>Continents</Nav.Link>
-                <Nav.Link href={process.env.PUBLIC_URL + '/countries'} className='tabs-section'>Countries</Nav.Link>
-                <Nav.Link href={process.env.PUBLIC_URL + '/news'} className='tabs-section'>News</Nav.Link>
-                <Nav.Link href={process.env.PUBLIC_URL + "/county"} className='tabs-section'>Local County Info</Nav.Link>
-                <Nav.Link href={process.env.PUBLIC_URL + "/usstates"} className='tabs-section'>US States</Nav.Link>
-                <Nav.Link href={process.env.PUBLIC_URL + "/testing-sites"} className='tabs-section'>Testing Sites</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + `/#/countries`} className='tabs-section'>Countries</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + `/#/news`} className='tabs-section'>News</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + `/#/county`} className='tabs-section'>Local County Info</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + `/#/usstates`} className='tabs-section'>US States</Nav.Link>
+                <Nav.Link href={process.env.PUBLIC_URL + `/#/testing-sites`} className='tabs-section'>Testing Sites</Nav.Link>
                 </Nav>
                 <Form inline>
                 {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" />
